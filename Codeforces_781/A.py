425eefe66dbe3be404e74375045fa04be04b7109
NintailsTF <2455717@dundee.ac.uk>
2022-04-08 17:03:16 +0100

    Completed 3.9 solution to the problem

---
+++ Codeforces_781/A.py
@@ -0,0 +1,85 @@
+from math import gcd
+from math import lcm
+
+
+
+def divideInt(problems):
+    toPrint = []
+    for i in range(problems):
+        num = int(input())
+        # Getting our number as 4 integer parts
+        result = ([num // 4 + (1 if x < num % 4 else 0) for x in range(4)])
+        # getting equal gcd and lcm from the array of numbers.
+        solution = gcdAndLcm(result)
+        toPrint.append(solution)
+    # print(toPrint)
+    # Printing the solutions after calculaStions
+    for i in toPrint:
+        for j in i:
+            print(j, end=" ")
+        print()
+
+
+
+def gcdAndLcm(numbers):
+    # print(numbers)
+    foundSolution = False
+    gcdNum = 0
+    lcmNum = 0
+    while foundSolution == False:
+        gcdNum = gcd(numbers[0], numbers[1])
+        lcmNum = lcm(numbers[2], numbers[3])
+        if gcdNum != lcmNum:
+            # If gcd is too small, bring a number from right to left.
+            if gcdNum > lcmNum:
+                if numbers[2] != 1 and numbers[3] != 1:
+                    # If position 2 in the array is 1
+                    if numbers[2] != 1:
+                        numbers[2] -= 1
+                        numbers[0] += 1
+                        gcdNum = gcd(numbers[0], numbers[1])
+                        lcmNum = lcm(numbers[2], numbers[3])
+                        if gcdNum == lcmNum:
+                            # print(gcdNum, lcmNum, numbers)
+                            break
+                    if numbers[3] != 1:
+                        numbers[3] -= 1
+                        numbers[0] += 1
+                        # Check the gcd again.
+                        gcdNum = gcd(numbers[0], numbers[1])
+                        lcmNum = lcm(numbers[2], numbers[3])
+                        if gcdNum == lcmNum:
+                            # print(gcdNum, lcmNum, numbers)
+                            break
+            elif lcmNum > gcdNum:
+                if numbers[2] != 1 and numbers[3] != 1:
+                    # If position 2 in the array is 1
+                    if numbers[2] != 1:
+                        numbers[2] -= 1
+                        numbers[0] += 1
+                        gcdNum = gcd(numbers[0], numbers[1])
+                        lcmNum = lcm(numbers[2], numbers[3])
+                        if gcdNum == lcmNum:
+                            # Leave instantly.
+                            # print(gcdNum, lcmNum, numbers)
+                            break
+                    if numbers[3] != 1:
+                        numbers[3] -= 1
+                        numbers[0] += 1
+                        # Check the gcd again.
+                        gcdNum = gcd(numbers[0], numbers[1])
+                        lcmNum = lcm(numbers[2], numbers[3])
+                        if gcdNum == lcmNum:
+                            # print(gcdNum, lcmNum, numbers)
+                            break
+        else:
+            foundSolution = True
+    # saving the solution
+    return [int(numbers[0]),int(numbers[1]),int(numbers[2]),int(numbers[3])]
+
+def main():
+    numberOfProblems = int(input())
+    divideInt(numberOfProblems)
+
+if __name__ == "__main__":
+    main()
