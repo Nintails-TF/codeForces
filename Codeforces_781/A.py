83c789fa6d6a672383fd958210d968fb51e21141
NintailsTF <2455717@dundee.ac.uk>
2022-04-08 17:17:16 +0100

    Added Question Statement as comment

--- Codeforces_781/A.py
+++ Codeforces_781/A.py
@@ -1,6 +1,33 @@
 from math import gcd
 from math import lcm
 
+"""
+You are given a positive integer n. You have to find 4 positive integers a,b,c,d
+
+such that
+
+    a+b+c+d=n
+and
+    gcd(a,b)=lcm(c,d)
+
+If there are several possible answers you can output any of them. It is possible to show that the answer always exists.
+
+In this problem gcd(a,b)
+denotes the greatest common divisor of a and b, and lcm(c,d) denotes the least common multiple of c and d
+
+Input
+
+The input consists of multiple test cases. The first line contains a single integer t
+(1≤t≤104) — the number of test cases. Description of the test cases follows.
+
+Each test case contains a single line with integer n
+(4≤n≤109) — the sum of a, b, c, and d
+
+Output
+
+For each test case output 4
+positive integers a, b, c, d such that a+b+c+d=n and gcd(a,b)=lcm(c,d).
+"""
 
 
 def divideInt(problems):
