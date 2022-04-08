15a7c7e4384ee49b83947fdb66006f2cbcde10b7
NintailsTF <2455717@dundee.ac.uk>
2022-04-08 20:47:52 +0100

    Wrote up initial solution statement

--- 800s/nextRound.py
+++ 800s/nextRound.py
@@ -4,13 +4,17 @@
 A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
 
 Input:
-
 The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
 
 The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
 
 Output:
-
 Output the number of participants who advance to the next round.
+
+Solution:
+We can see that k = number of participants, n = the minimum score needed to progress. What we want to do is loop for k, and then
+check if the particpant has a score greater than n, if so they move to the next round otherwise we discard them.
+
+
 
 """
